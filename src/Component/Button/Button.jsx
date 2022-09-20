ccfcfc2c0450a368be10ccd0029f3849abe6b981
dheerendra143 <singhdheerendra83@gmail.com>
2022-09-20 19:30:46 +0530

    test case

---
+++ src/Component/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+    const handleClick = (e) => {
+        fetch(`https://demo.dataverse.org/api/search?q=apple`).then(res => res.json()).then(res => {
+            console.log("result", res)
+        })
+    }
+    return (
+        <button onClick={(e)=>handleClick(e)}>Click here</button>
+    )
+}
+
+export default Button;
---
+++ src/__tests__/Componets/Button.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../../Component/Button/Button';
+
+test('renders learn react link', () => {
+  render(<Button />);
+//   const linkElement = screen.getByTestId("");
+//   expect(linkElement).toBeInTheDocument();
+//   expect(linkElement).toMatchSnapshot();
+});
